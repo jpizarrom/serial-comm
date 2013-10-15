f77104eb45589638c6a0f4d77e47c17db6f1605e
will.hedgecock@gmail.com <will.hedgecock@gmail.com@fad24c15-23f3-440c-4f30-9bac9bc21022>
2013-10-15 14:55:37 +0000

    Added LGPL License statements.

--- j/extensions/comm/SerialComm.java
+++ j/extensions/comm/SerialComm.java
@@ -1,3 +1,28 @@
+/*
+ * SerialComm.java
+ *
+ *       Created on:  Feb 25, 2012
+ *  Last Updated on:  Mar 14, 2013
+ *           Author:  Will Hedgecock
+ *
+ * Copyright (C) 2012-2013 Will Hedgecock
+ *
+ * This file is part of SerialComm.
+ *
+ * SerialComm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SerialComm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SerialComm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package j.extensions.comm;
 
 import java.io.File;
